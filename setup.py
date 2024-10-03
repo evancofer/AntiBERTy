6a74d9c26ab352701e97d7c4a8b9b150f1f99cc5
evancofer <evan@asimov.com>
2024-10-03 11:29:22 -0400

    Update build

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ import tarfile
 import urllib.request
 import shutil
 import setuptools
-from setuptools.command.install import install
+from setuptools.command.build import build
 
 
-class DownloadModelCommand(install):
+class CustomBuildCommand(build):
     def run(self):
-        install.run(self)
         url = "https://files.pythonhosted.org/packages/a1/3b/2cf48ec21956252fdc5c5dd1b7f8bb8b12f5208bd3eaaad412ced3ed0ff5/antiberty-0.1.3.tar.gz"
         urllib.request.urlretrieve(url, "./antiberty-0.1.3.tar.gz")
         with tarfile.open("antiberty-0.1.3.tar.gz", "r:gz") as tar:
@@ -16,7 +15,8 @@ class DownloadModelCommand(install):
         shutil.move("antiberty-0.1.3/antiberty/trained_models", "antiberty/trained_models")
         shutil.rmtree("antiberty-0.1.3")
         os.remove("./antiberty-0.1.3.tar.gz")
+        build.run(self)
 
 
 
-setuptools.setup(cmdclass={"install": DownloadModelCommand})
+setuptools.setup(cmdclass={"build": CustomBuildCommand})
